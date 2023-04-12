a974d7df5da880a492d01152c03392ac2bf23070
Licini <leochen1990@gmail.com>
2023-04-12 16:00:40 +0200

    deal with OCC's knot and multiplicity simplification

--- src/compas_ifc/resources/brep.py
+++ src/compas_ifc/resources/brep.py
@@ -12,6 +12,20 @@ from .primities import frame_to_ifc_plane
 from .shapes import occ_cylinder_to_ifc_cylindrical_surface
 
 
+def calculate_knots_and_multiplicities(knot_sequence):
+    knots = [knot_sequence[0]]
+    multiplicities = [1]
+
+    for i in range(1, len(knot_sequence)):
+        if knot_sequence[i] != knot_sequence[i - 1]:
+            knots.append(knot_sequence[i])
+            multiplicities.append(1)
+        else:
+            multiplicities[-1] += 1
+
+    return knots, multiplicities
+
+
 def brep_to_ifc_advanced_brep(file: ifcopenshell.file, brep: BRep) -> List[ifcopenshell.entity_instance]:
     brep.fix()
     brep.sew()
@@ -52,42 +66,28 @@ def brep_to_ifc_advanced_brep(file: ifcopenshell.file, brep: BRep) -> List[ifcop
                 end_vertex = file.create_entity("IfcVertexPoint", get_ifc_point(edge.last_vertex.point))
 
             curve = edge.nurbscurve
-            multiplicities = curve.multiplicities
-
             control_points = [get_ifc_point(point) for point in curve.points]
+            weights = curve.weights
+
+            # OCC will simplify the knot and multiplicity when the curver or surface is periodic,
+            # so we need to recalculate the knot and multiplicity from the knot sequence.
+            # Additonally, we will need to add back the duplicated control point and weight value.
+            knots, multiplicities = calculate_knots_and_multiplicities(curve.knotsequence)
             if curve.is_closed:
                 control_points.append(control_points[0])
-                # TODO: this is not correct when curve is rebuilt
-                multiplicities[0] += 1
-                multiplicities[-1] += 1
+                weights.append(weights[0])
 
-            if curve.is_rational:
-                weights = curve.weights
-                if curve.is_closed:
-                    weights = weights + [weights[0]]
-
-                IfcBSpline = file.create_entity(
-                    "IfcRationalBSplineCurveWithKnots",
-                    Degree=curve.degree,
-                    ControlPointsList=control_points,
-                    CurveForm="UNSPECIFIED",
-                    ClosedCurve=curve.is_closed,
-                    SelfIntersect=False,
-                    KnotMultiplicities=multiplicities,
-                    Knots=curve.knots,
-                    WeightsData=weights,
-                )
-            else:
-                IfcBSpline = file.create_entity(
-                    "IfcBSplineCurveWithKnots",
-                    Degree=curve.degree,
-                    ControlPointsList=control_points,
-                    CurveForm="UNSPECIFIED",
-                    ClosedCurve=curve.is_closed,
-                    SelfIntersect=False,
-                    KnotMultiplicities=multiplicities,
-                    Knots=curve.knots,
-                )
+            IfcBSpline = file.create_entity(
+                "IfcRationalBSplineCurveWithKnots",
+                Degree=curve.degree,
+                ControlPointsList=control_points,
+                CurveForm="UNSPECIFIED",
+                ClosedCurve=curve.is_closed,
+                SelfIntersect=False,
+                KnotMultiplicities=multiplicities,
+                Knots=knots,
+                WeightsData=weights,
+            )
 
             IfcEdgeCurve = file.create_entity(
                 "IfcEdgeCurve",
@@ -163,24 +163,31 @@ def brep_to_ifc_advanced_brep(file: ifcopenshell.file, brep: BRep) -> List[ifcop
                         ifc_face_bound = file.create_entity("IfcFaceBound", edge_loop, True)
                     face_bounds.append(ifc_face_bound)
 
+                same_sense = face.orientation == 0
                 if face.is_plane:
                     occ_plane = face.occ_adaptor.Plane()
                     frame = occ_plane_to_frame(occ_plane)
                     ifc_plane = frame_to_ifc_plane(file, frame)
-                    IfcAdvancedFace = file.create_entity("IfcAdvancedFace", face_bounds, ifc_plane, SameSense=True)
+                    IfcAdvancedFace = file.create_entity(
+                        "IfcAdvancedFace", face_bounds, ifc_plane, SameSense=same_sense
+                    )
                 elif face.is_cylinder:
                     cylinder = face.occ_adaptor.Cylinder()
                     IfcCylindricalSurface = occ_cylinder_to_ifc_cylindrical_surface(file, cylinder)
                     IfcAdvancedFace = file.create_entity(
-                        "IfcAdvancedFace", face_bounds, IfcCylindricalSurface, SameSense=True
+                        "IfcAdvancedFace", face_bounds, IfcCylindricalSurface, SameSense=same_sense
                     )
                 else:
                     control_points = np.array(face.nurbssurface.points.points, dtype=float)
                     control_points = control_points.swapaxes(0, 1)
                     ifc_control_points = []
 
-                    u_mults = face.nurbssurface.u_mults
-                    v_mults = face.nurbssurface.v_mults
+                    u_knots, u_mults = calculate_knots_and_multiplicities(
+                        list(face.nurbssurface.occ_surface.UKnotSequence())
+                    )
+                    v_knots, v_mults = calculate_knots_and_multiplicities(
+                        list(face.nurbssurface.occ_surface.VKnotSequence())
+                    )
 
                     for row in control_points:
                         ifc_row = []
@@ -193,14 +200,10 @@ def brep_to_ifc_advanced_brep(file: ifcopenshell.file, brep: BRep) -> List[ifcop
                         for point in control_points[0]:
                             new_row.append(get_ifc_point(point))
                         ifc_control_points.append(new_row)
-                        u_mults[0] += 1
-                        u_mults[-1] += 1
 
                     if face.nurbssurface.is_v_periodic:
                         for i, row in enumerate(ifc_control_points):
                             row.append(get_ifc_point(control_points[i][0]))
-                        v_mults[0] += 1
-                        v_mults[-1] += 1
 
                     weights = face.nurbssurface.weights
                     weights = np.array(weights, dtype=float)
@@ -230,13 +233,13 @@ def brep_to_ifc_advanced_brep(file: ifcopenshell.file, brep: BRep) -> List[ifcop
                         SelfIntersect=False,  # Seems no way to get this from OCC
                         UMultiplicities=u_mults,
                         VMultiplicities=v_mults,
-                        UKnots=face.nurbssurface.u_knots,
-                        VKnots=face.nurbssurface.v_knots,
+                        UKnots=u_knots,
+                        VKnots=v_knots,
                         WeightsData=ifc_weights,
                     )
 
                     IfcAdvancedFace = file.create_entity(
-                        "IfcAdvancedFace", face_bounds, IfcBSplineSurfaceWithKnots, SameSense=True
+                        "IfcAdvancedFace", face_bounds, IfcBSplineSurfaceWithKnots, SameSense=same_sense
                     )
 
                 ifc_faces.append(IfcAdvancedFace)
