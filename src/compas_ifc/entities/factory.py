d417c1bc8dd439fe5a6a9cd3d7ff627cd8c17d36
Licini <leochen1990@gmail.com>
2023-08-07 17:04:27 +0200

    extension

--- src/compas_ifc/entities/factory.py
+++ src/compas_ifc/entities/factory.py
@@ -1,22 +1,42 @@
 import ifcopenshell
 
-# TODO: use polymorphism to simplify the class inheritance.
 # TODO: pre-generate all classes.
+# TODO: separate ifcopenshell APIs
 
 class Factory:
+
+    EXTENSIONS = {}
 
     def __init__(self, schema: str = "IFC4") -> None:
         self.schema = ifcopenshell.ifcopenshell_wrapper.schema_by_name(schema)
     
+    def get_extensions(self, name, declaration):
+        name = declaration.name()
+        inheritances = [name]
+        supertype = declaration.supertype()
+        while supertype:
+            name = supertype.name()
+            inheritances.append(name)
+            supertype = supertype.supertype()
+        
+        inheritances = inheritances[::-1]
+        extensions = []
+        for cls in inheritances:
+            if cls in self.EXTENSIONS:
+                extensions.append(self.EXTENSIONS[cls])
+        return tuple(extensions)
+
     def create_class(self, name):
 
         declaration = self.schema.declaration_by_name(name)
+        extensions = self.get_extensions(name, declaration)
 
         def init(self, entity):
             self._entity = entity
 
         attributes = {
             "__init__": init,
+            "__repr__": lambda self: f"<compas_ifc.{self._entity.is_a()}>",
         }
 
         for attribute in declaration.all_attributes():
@@ -26,8 +46,8 @@ class Factory:
         for attribute in declaration.all_inverse_attributes():
             name = attribute.name()
             attributes[name] = self.create_getter(attribute, setter=False)
-        
-        return type(name, (), attributes)
+
+        return type(name, extensions, attributes)
 
     def create_getter(self, attribute, setter=True):
 
@@ -43,6 +63,9 @@ class Factory:
                 if not hasattr(self._entity, name):
                     raise AttributeError(f"Attribute <{name}> does not exist")
 
+                # Make checks pluggable functions
+
+                # check aggregation type
                 attribute_type = attribute.type_of_attribute()
                 if attribute_type.as_aggregation_type():
                     value = list(value)
@@ -53,6 +76,7 @@ class Factory:
                             raise ValueError(f"Expected <{name}> to have between {lower} and {upper} elements")
                     # TODO: check type of elements
 
+                # check entity type
                 if attribute_type.as_named_type():
                     declared_type = attribute_type.declared_type()
                     type_name = declared_type.name()
@@ -68,16 +92,24 @@ class Factory:
 if __name__ == "__main__":
 
     file = ifcopenshell.open("data/wall-with-opening-and-window.ifc")
-    _entity = file[3]
+    _entity = file[34]
     declaration = _entity.is_a()
+
+    from extensions import EXTENSIONS
+    Factory.EXTENSIONS = EXTENSIONS
+    print(Factory.EXTENSIONS)
 
     factory = Factory()
     Entity = factory.create_class(declaration)
 
     entity = Entity(_entity)
-    print(entity.ThePerson)
-    # entity.ThePerson = "some wrong value type"
-    entity.ThePerson = file[3]
-
-    print(entity.ThePerson)
+    entity.test()
+    entity.test2()
     print(entity)
+
+    # print(entity.ThePerson)
+    # # entity.ThePerson = "some wrong value type"
+    # entity.ThePerson = file[3]
+
+    # print(entity.ThePerson)
+    # print(entity)
