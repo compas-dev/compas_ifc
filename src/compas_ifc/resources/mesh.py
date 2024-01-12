8d5cd1939caaf08ab4dfd7354946248b8c28197b
Li Chen <leochen1990@gmail.com>
2024-01-12 18:52:00 +0100

    fix vertex index bug

--- src/compas_ifc/resources/mesh.py
+++ src/compas_ifc/resources/mesh.py
@@ -7,12 +7,17 @@ def mesh_to_IfcPolygonalFaceSet(file: ifcopenshell.file, mesh: Mesh) -> ifcopens
     """
     Convert a COMPAS mesh to an IFC PolygonalFaceSet.
     """
-    vertices = []
-    for _, attr in mesh.vertices(True):
-        vertices.append((float(attr["x"]), float(attr["y"]), float(attr["z"])))
+    _vertices = []
+    for key, attr in mesh.vertices(True):
+        _vertices.append((key, (float(attr["x"]), float(attr["y"]), float(attr["z"]))))
+    _vertices = sorted(_vertices, key=lambda x: x[0])
+    vertices = [v[1] for v in _vertices]
+
+    keys = [v[0] for v in _vertices]
+
     faces = []
     for fkey in mesh.faces():
-        indexes = [i + 1 for i in mesh.face_vertices(fkey)]
+        indexes = [keys.index(i) + 1 for i in mesh.face_vertices(fkey)]
         faces.append(file.createIfcIndexedPolygonalFace(indexes))
 
     return file.create_entity(
