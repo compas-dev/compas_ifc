244f0c1199ca40a3735e7783aec0190f64fd79dd
Licini <leochen1990@gmail.com>
2024-07-22 16:54:19 +0200

    fix issue assigning existing property

--- src/compas_ifc/entities/extensions/IfcObject.py
+++ src/compas_ifc/entities/extensions/IfcObject.py
@@ -1,5 +1,6 @@
 from typing import TYPE_CHECKING
 
+import ifcopenshell.guid
 from ifcopenshell.api import run
 from ifcopenshell.util.element import get_psets
 from ifcopenshell.util.element import get_quantities
@@ -19,14 +20,20 @@ class IfcObject(IfcObject):
 
     @properties.setter
     def properties(self, psets):
+        if id(self.file) not in self._psetsmap:
+            self._psetsmap[id(self.file)] = {}
+
+        psetsmap = self._psetsmap[id(self.file)]
+
         for name, properties in psets.items():
-            if id(properties) in self._psetsmap:
-                pset = self._psetsmap[id(properties)]
+            if id(properties) in psetsmap:
+                pset = psetsmap[id(properties)]
                 # TODO: Check if relation already exists
-                self.file.create_entity("IfcRelDefinesByProperties", GlobalId=self.create_guid(), RelatingPropertyDefinition=pset, RelatedObjects=[self.entity])
+                self.file._create_entity("IfcRelDefinesByProperties", GlobalId=ifcopenshell.guid.new(), RelatingPropertyDefinition=pset, RelatedObjects=[self.entity])
+
             else:
                 pset = run("pset.add_pset", self.file._file, product=self.entity, name=name)
-                self._psetsmap[id(properties)] = pset
+                psetsmap[id(properties)] = pset
                 for key, value in properties.items():
                     if not isinstance(value, (str, int, float)):
                         properties[key] = str(value)
