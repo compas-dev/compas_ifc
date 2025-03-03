bd916d46cd6a48d57d94886cee3123291e53b7b7
Licini <leochen1990@gmail.com>
2025-03-03 11:08:40 +0100

    drafts

---
+++ src/compas_ifc/entities/buildingelements.py
@@ -0,0 +1,84 @@
+from compas_model.models import Model
+from compas_model.elements import Element
+
+
+# Use pydantic class for auto-generated IFC classes??
+
+
+
+class BuildingModel(Model):
+    # This class will maintain and update a IFC file underneath.
+
+    def __init__(self, **kwargs):
+
+        # The model will start an empty IFC file if not provided.
+        super().__init__(**kwargs)
+        self._building_elements = []
+        self._ifc_project = None # The underlying IFC project (we don't treat it as element here)
+
+    @classmethod
+    def from_ifc(self, ifc_file):
+        pass
+
+    @classmethod
+    def from_template(self, building_count=1, storey_count=1):
+        # Do we support multiple sites?
+        pass
+
+
+
+class BuildingElement(Element):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+        self._ifc_entity = None # Read-only, underlying IFC entity.
+        self._ifc_class = None # Read-only, IFC class name, maybe redundant.
+
+
+        self.ifc_attributes = {} # View object, mimics dict but will update the underlying IFC entity when changed, it also validates againts the IFC schema.
+        self.ifc_property_sets = {} # View object, the parametric definition will be added here too.
+
+        # Common attributes of all building elements.
+        self._name = None
+        self._description = None
+
+        self.geometry = None
+        # Option1: "Baked geometry", Brep (Tessellated or Full).
+        # Option2a: "Parametric geometry", this class inherrits from another like `Beam`, this might branching out to too many classes.
+        # Option2b: "Parametric geometry", this refers to a class like `Beam` as attribute, this makes the class structure convoluted.
+
+        # The question becomes: 
+        # 1.should the concept of "geometry" be detached from "Element"? which is the case of IFC.
+        # 2.Data flow-wise, the data should always flow from Model to IFC, we will not likely modify geometry if IFC elements, but what happens when we read it back?
+        # 3.How do we convert baked geometry back to a parametric definition? (we save the parametric definition in PSet)
+        # 4.If we want to make update, do we convert the whole model back to a parametric (Block) model, or should we update the BuildingModel in-place?
+
+
+        self.material = None # There needs to be some mapping mechanism between the material class and Qtos.
+
+
+    @classmethod
+    def from_ifc(cls, ifc_entity):
+        # From a raw IFC entity fill-in all the attributes and setup the links etc.
+        pass
+
+    def add(self, **kwargs):
+        super().add(**kwargs)
+        # also update the IFC file
+        pass
+
+    def remove(self, **kwargs):
+        super().remove(**kwargs)
+        # also update the IFC file
+        pass
+
+    @property
+    def transformation(self):
+        # Get the transformation matrix of the element
+        pass
+
+    @transformation.setter
+    def transformation(self, transformation):
+        # Set the transformation matrix of the element, will also update the placement of the underlying IFC entity.
+        pass
+
