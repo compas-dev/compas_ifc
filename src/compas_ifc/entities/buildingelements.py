d25ee5a20c1217be6a820a8cb97bc9cbb1b2be41
Li Chen <leochen1990@gmail.com>
2025-03-11 12:41:21 +0100

    more notes

--- src/compas_ifc/entities/buildingelements.py
+++ src/compas_ifc/entities/buildingelements.py
@@ -2,9 +2,6 @@ from compas_model.models import Model
 from compas_model.elements import Element
 
 
-# Use pydantic class for auto-generated IFC classes??
-
-
 
 class BuildingModel(Model):
     # This class will maintain and update a IFC file underneath.
@@ -13,8 +10,8 @@ class BuildingModel(Model):
 
         # The model will start an empty IFC file if not provided.
         super().__init__(**kwargs)
-        self._building_elements = []
         self._ifc_project = None # The underlying IFC project (we don't treat it as element here)
+        # How about site? Maybe also integrate it here.
 
     @classmethod
     def from_ifc(self, ifc_file):
@@ -25,6 +22,24 @@ class BuildingModel(Model):
         # Do we support multiple sites?
         pass
 
+    @classmethod
+    def from_blockmodel(self, blockmodel, mapping=None):
+        # Create a building model from a blockmodel.
+        # mapping is a dictionary that maps the blockmodel elements to IFC elements.
+        # TODO: think about the role of different models.
+        pass
+
+    # To ...
+
+    def add(self, **kwargs):
+        super().add(**kwargs)
+        # also update the IFC file
+        pass
+
+    def remove(self, **kwargs):
+        super().remove(**kwargs)
+        # also update the IFC file
+        pass
 
 
 class BuildingElement(Element):
@@ -33,10 +48,18 @@ class BuildingElement(Element):
 
         self._ifc_entity = None # Read-only, underlying IFC entity.
         self._ifc_class = None # Read-only, IFC class name, maybe redundant.
+        # There are 30 IfcBuiling(/t)Elements, but they only add one or two more attributes and all of them OPTIONAL.
+        # The value of adding these classes are questionable. 
+        # The definition of these classes can be sometimes conflited between fields, for example Beams in architecrual and structrual context. 
+        # At least in IFC cases, these classification are perhaps better expressed as "Labels" instead of branched-out classes.
+
 
 
         self.ifc_attributes = {} # View object, mimics dict but will update the underlying IFC entity when changed, it also validates againts the IFC schema.
         self.ifc_property_sets = {} # View object, the parametric definition will be added here too.
+        # Should we abstract away these two, and have them managed together by a single JSON schema?
+        # The artificial division between attributes and property sets is also a reason of "rogue customizetion". It also confuse with Python's interperation of these two words.
+        # The combined name being?
 
         # Common attributes of all building elements.
         self._name = None
@@ -53,13 +76,28 @@ class BuildingElement(Element):
         # 3.How do we convert baked geometry back to a parametric definition? (we save the parametric definition in PSet)
         # 4.If we want to make update, do we convert the whole model back to a parametric (Block) model, or should we update the BuildingModel in-place?
 
+        # Prefered: single class with consistant APIs that handles mapping to/from IFC classes.
 
-        self.material = None # There needs to be some mapping mechanism between the material class and Qtos.
+        self.material = None # There needs to be some mapping mechanism between the material class and Qtos in IFC.
+
+        # How about storeys? Maybe as a calculated property of the building elements?
+
+
+        # links to parents
+        self.storey = None
+        self.building = None
+        self.model = None
 
 
     @classmethod
     def from_ifc(cls, ifc_entity):
         # From a raw IFC entity fill-in all the attributes and setup the links etc.
+        pass
+
+    @classmethod
+    def from_blockelement(cls, blockelement, mapping=None):
+        # Create a building element from a blockelement.
+        # mapping is a dictionary that maps the blockelement elements to IFC elements.
         pass
 
     def add(self, **kwargs):
@@ -82,3 +120,8 @@ class BuildingElement(Element):
         # Set the transformation matrix of the element, will also update the placement of the underlying IFC entity.
         pass
 
+
+
+# Use pydantic class for auto-generated IFC classes?? How does it deal with pointers in seriliasition?
+
+# TODO: Transparent way to work with grid and storeys etc.
