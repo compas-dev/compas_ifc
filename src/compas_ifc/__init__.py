c1c2fec1d27c699771f226d706928bd612b12a83
Li <leochen1990@gmail.com>
2026-07-13 11:12:23 +0200

    Bump version to 2.1.0

--- src/compas_ifc/__init__.py
+++ src/compas_ifc/__init__.py
@@ -37,7 +37,7 @@ __author__ = ["Li Chen", "Tom Van Mele"]
 __copyright__ = "ETH Zurich"
 __license__ = "MIT License"
 __email__ = "li.chen@arch.ethz.ch"
-__version__ = "2.0.0"
+__version__ = "2.1.0"
 
 
 HERE = os.path.dirname(__file__)
